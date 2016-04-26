b264d7f1ccd4fb7a94f0044b9309ed89ae2252e1
Alba <albagallegomontejo95@gmail.com>
2016-04-26 23:37:40 +0200

    Tema 3:Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+var quizController = require('../controllers/quiz_controller');
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
 router.get('/author',function(req,res){
-
 res.render('author',{title:'AUTORES' });
 });
+
+
+router.get('/question',quizController.question);
+router.get('/check',quizController.check);
+
 
 module.exports = router;
