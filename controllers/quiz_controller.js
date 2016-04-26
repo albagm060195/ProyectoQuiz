18c2e6ec609674ba7533d41e5c510b2ea918e067
Alba <albagallegomontejo95@gmail.com>
2016-04-26 23:53:57 +0200

    Tema 3:Primera pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,15 @@
-exports.question= function(req,res,next){
 
-res.render('quizzes/question',{question: 'Capital de Italia'});
+
+exports.question= function(req,res,next){
+var answer = req.query.answer || "" ;
+res.render('quizzes/question',{question: 'Capital de Italia',answer: answer});
 
 };
 
 exports.check = function(req,res,next){
 
-
-var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta' ;
-res.render('quizzes/result',{result: result});
+var answer = req.query.answer || '' ;
+var result = ((req.query.answer === 'Roma') ? 'Correcta' : 'Incorrecta') ;
+res.render('quizzes/result',{result: result,answer: answer});
 
 };
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index',{title:'Quiz' });
 });
 
 router.get('/author',function(req,res){
@@ -14,8 +14,8 @@ res.render('author',{title:'AUTORES' });
 });
 
 
-router.get('/question',quizController.question);
-router.get('/check',quizController.check);
+router.get('/question',require('../controllers/quiz_controller').question);
+router.get('/check', quizController.check);
 
 
 module.exports = router;
