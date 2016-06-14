2aaa9b6a0b0fa02d77b1239512d5c7b3948f10af
alba gallego montejo <albagallegomontejo95@gmail.com>
2016-06-14 09:54:10 +0200

    DespuesDeMigrateySeeder

--- models/index.js
+++ models/index.js
@@ -18,19 +18,4 @@ var sequelize = new Sequelize(url,{ storage:storage,omitNull:true });
 
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
-
-
-
-
-sequelize.sync().then(function() { return Quiz.count().then(function(c) {
-if (c === 0) {
-return Quiz.bulkCreate([{question: 'Capital de Italia', answer: 'Roma' },{question: 'Capital de Portugal', answer: 'Lisboa' }]).then(function(){
-console.log('Base de datos inicializada con datos');
-});
-}
-});
-}).catch(function(error) {
-console.log("Error Sincronizando las tablas de la BBDD: ", error);
-process.exit(1);
-});
 exports.Quiz = Quiz;
