20bf3420ee5cb643215a7d1871f61705d56686bb
alba gallego montejo <albagallegomontejo95@gmail.com>
2016-06-15 01:50:32 +0200

    Tema 20

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var userController = require('../controllers/user_controller');
 
 router.get('/', function(req, res, next) {
   res.render('index',{title:'Quiz' });
@@ -16,6 +17,7 @@ res.render('author',{title:'AUTORES' });
 
 //Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('userId', userController.load); // autoload :userId
 
 //Definición de rutas
 
@@ -27,8 +29,19 @@ router.post('/quizzes',quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',quizController.destroy);
+
+//Comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',commentController.create);
+
+// Definición de rutas de cuenta
+router.get('/users',                    userController.index);   // listado usuarios
+router.get('/users/:userId(\\d+)',      userController.show);    // ver un usuario
+router.get('/users/new',                userController.new);     // formulario sign un
+router.post('/users',                   userController.create);  // registrar usuario
+router.get('/users/:userId(\\d+)/edit', userController.edit);
+router.put('/users/:userId(\\d+)',	userController.update);
+router.delete('/users/:userId(\\d+)',	userController.destroy);
 
 
 module.exports = router;
