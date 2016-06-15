02881ce07956c55b5f0bf0e5741534ff642a68b4
alba gallego montejo <albagallegomontejo95@gmail.com>
2016-06-15 09:34:34 +0200

    tema 24

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ res.render('author',{title:'AUTORES' });
 //Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load); //autoload :quizId
 router.param('userId', userController.load); // autoload :userId
+router.param('commentId', commentController.load); //autoload :commentId
 
 //Definición de rutas
 
@@ -34,6 +35,7 @@ router.delete('/quizzes/:quizId(\\d+)',sessionController.loginRequired,quizContr
 //Comentarios
 router.get('/quizzes/:quizId(\\d+)/comments/new',sessionController.loginRequired, commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',sessionController.loginRequired,commentController.create);
+router.put('/quizzes/:quizId(\\d+)/comments/:commentId(\\d+)/accept', sessionController.loginRequired,commentController.accept);
 
 // Definición de rutas de cuenta
 router.get('/users',                    userController.index);   // listado usuarios
